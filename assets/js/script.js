af4a8b090a87ab65f4ad17760651cc3e1219eeff
ti-k-sper <y.chargueraud@hotmail.fr>
2019-05-22 16:22:30 +0200

    etape 2 index ok article ok allarticle ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@ var text = document.getElementsByClassName("textArticle");
 //console.log(text);
 for (var i = 0; i < text.length; i++) {
 	//console.log(text[i]);
-	text[i].innerHTML = text[i].innerHTML.substring(0, 300);
+	text[i].innerHTML = text[i].innerHTML.substring(0, 270);
 }
